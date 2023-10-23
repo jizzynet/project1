182e93aa6738fe58ff00f8da15e7e68353eab6a4
john <johnegbarevba14@gmail.com>
2023-10-23 11:07:09 +0100

    hi

---
+++ index.js
@@ -0,0 +1,179 @@
+// const john = 'my name is john';
+// console.log(john
+// number data type
+// let shoesize = 30;
+// let clothsize= 25;
+// let totalsum = shoesize + clothsize;
+// alert(totalsum);
+// string datatype
+// let music = `counrty music`;
+// let hobby = `coding`;
+// let age = 17;
+// let info = `my best song ${music} and i love ${hobby} and i am ${age} years old`
+// alert(info)
+// let message = "hey there this \n string data base"
+// alert(message.toUpperCase())
+// alert(message.startsWith(hey))
+ 
+// numberdatatype
+// let Malepopulation= 50;
+// let Femalepopulation= 60;
+// let totalpopulation = Malepopulation + Femalepopulation
+// alert(totalpopulation);
+
+// string data type
+// 
+
+// let message = "hi just landed last week \n and thought i should come and visit u "
+// alert(message.toUpperCase())
+// alert(message.startsWith(hi))
+
+// ARRAYS 
+// let fruits = ["oranges", "mangoes", "cherry", "apple", ]
+// let john = fruits.map(dami=>dami.toUpperCase());
+// alert(john);
+// // fruits.shift();
+// // fruits.unshift("pineapple");
+// // alert(fruits);
+// // alert(fruits.length);
+// fruits.delete("mangoes");
+// alert(fruits);
+
+
+// LOOP
+
+// let width = 40
+// while(width<130){
+//     console.log(width)
+// //     width++
+// }
+// let mystring = "astey$567";
+// let pwlen = mystring.length;
+// let chlen = 0;
+// let mych = " ";
+// while(chlen <= pwlen){
+//     mych = mystring.substring(chlen,chlen + 1);
+// alert(mych); 
+// chlen++; 
+// }
+// if mystring 
+
+  
+// let pas = "johnsunday099$"
+// let pwlen = pas.length
+// let char = 0
+// let mystring = ""
+// let pwok =  false;
+// while(char <= pwlen){
+//     mystring = pas.substring(char, char+1)
+//     alert(mystring);
+//     if(mystring == "$"){
+//         pwok = true;
+//     }
+//     char++
+//     if(pwok == true){
+//         alert("there is dollarsign")
+//     }else{
+//         alert('there is no dollarsign')
+//     }
+    
+//    }
+//do while loop
+// let width = 120
+// do{
+//     console.log(width)
+//     width++
+// }
+//     while(width <150)
+
+
+
+// for loop
+// for(let v=0; v<10; v++ ){
+    // console.log(v)
+// }
+
+    // for eachloop
+//    let set  = [56, 65, 67, 78.8, 67.7]
+//    set.forEach(john=>{
+    // console.log(`The number is : ${john}`)
+//    })
+
+// for of loop
+// for(let john of set)
+// console.log(`The number is : ${john}`)
+
+// CONDITIONALS
+   
+// if statement
+// let day= "tuesday"
+// if(day == 'tuesday'){
+//     alert("Today is tuesday")
+// }
+
+// let shoesize = 24
+// if(shoe == 25){
+//     alert("size is available");
+// }else{
+//     alert("size is not available")
+// 
+// MULTIPLE
+// let day = "Tuesday"
+// Multiple if-else statement
+// if(day == "monday"){
+//     alert("Today is Monday")
+// }else if(day == "Tuesday"){
+//     alert("Today is Tuesday")
+// }else if (day == "Wednesday"){
+//     alert("Today is Wednesday")
+// }else if (day == Thursday ){
+//     alert("Todayis neither Monday, Tuesday, Wedensday")
+// }else{
+//     alert("Today is weekend")
+// }
+//  FUNCTIONS
+//    let boys = 50
+
+//    function gender(){
+//     let girls = 10
+//     total = boys + girls;
+//     girls = girls + 40;
+//     console.log("the sum of gender are",total)
+//     console.log("the sum of girls are", girls)
+//    }
+//    gender()
+
+//    let red = 200
+
+//    function colors(){
+//     let blue = 100
+//     let green = 300
+//     let purple = 200
+//     bricolours = red + blue + green;
+//     dacolurs = red + green + purple;
+//     console.log("the bright colurs are",bricolours)
+//     console.log("the darkcolours are",dacolurs)
+//    }
+//    colors()
+
+function clickme(){
+    document.getElementById("btn").innerHTML = john;
+}
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
